850a8264295811fa4a0f8be6b912c7c4234764b9
Jean-Claude Wippler <jc@wippler.nl>
2016-10-17 00:45:37 +0200

    add term and readline packages

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"os"
+	"bytes"
+
+	"github.com/chzyer/readline"
+	"github.com/pkg/term"
+)
 
 func main() {
-	fmt.Println("Hello from go!")
+	tty, _ := term.Open("/dev/cu.usbmodemD5D4C5E3",
+		term.Speed(115200), term.CBreakMode)
+
+	rl, _ := readline.NewEx(&readline.Config{
+		UniqueEditLine: true,
+	})
+	defer rl.Close()
+
+	go func() {
+		for {
+			line := make([]byte, 100)
+			n, _ := tty.Read(line)
+			line = bytes.Replace(line[:n], []byte("\n"), []byte("\r\n"), -1)
+			os.Stdout.Write(line)
+		}
+	}()
+
+	for {
+		line, err := rl.Readline()
+		if err != nil {
+			break
+		}
+		tty.Write([]byte(line + "\r"))
+	}
 }
