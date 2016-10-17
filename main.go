ff6cdbc8553a9db10c2c0646a78022b4626f595d
Jean-Claude Wippler <jc@wippler.nl>
2016-10-18 01:30:25 +0200

    expand main app structure a bit

--- main.go
+++ main.go
@@ -1,22 +1,33 @@
 package main
 
+//go:generate go-bindata data/
+
 import (
 	"bytes"
+	"flag"
+	"log"
 	"os"
+	"sync"
 
 	"github.com/chzyer/readline"
 	"github.com/tarm/serial"
 )
 
-func main() {
-	devname := "/dev/cu.usbmodemD5D4C5E3"
-	config := serial.Config{Name: devname, Baud: 115200}
-	tty, _ := serial.OpenPort(&config)
+var (
+	port = flag.String("p", "", "serial port (usually /dev/tty* or COM*)")
+	baud = flag.Int("b", 115200, "serial baud rate")
 
-	rl, _ := readline.NewEx(&readline.Config{
-		UniqueEditLine: true,
-	})
-	defer rl.Close()
+	tasks sync.WaitGroup
+	tty   *serial.Port
+)
+
+func main() {
+	var err error
+	flag.Parse()
+
+	config := serial.Config{Name: *port, Baud: *baud}
+	tty, err = serial.OpenPort(&config)
+	check(err)
 
 	go func() {
 		for {
@@ -27,11 +38,30 @@ func main() {
 		}
 	}()
 
+	tasks.Add(1)
+	go consoleTask()
+
+	tasks.Wait()
+}
+
+func consoleTask() {
+	defer tasks.Done()
+
+	rl, err := readline.NewEx(&readline.Config{ UniqueEditLine: true })
+	check(err)
+	defer rl.Close()
+
 	for {
 		line, err := rl.Readline()
 		if err != nil {
 			break
 		}
 		tty.Write([]byte(line + "\r"))
+	}
+}
+
+func check(e error) {
+	if e != nil {
+		log.Fatal(e)
 	}
 }
