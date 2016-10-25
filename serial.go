a30544cb20fc6f9a0e089824a6144be6dd7e2fb6
Jean-Claude Wippler <jc@wippler.nl>
2016-10-25 18:49:52 +0200

    include crc16 when listing built-in images, closes #16

--- serial.go
+++ serial.go
@@ -191,6 +191,20 @@ func wrappedSend(argv []string) {
 	}
 }
 
+var crcTable = []uint16{
+	0x0000, 0xCC01, 0xD801, 0x1400, 0xF001, 0x3C00, 0x2800, 0xE401,
+	0xA001, 0x6C00, 0x7800, 0xB401, 0x5000, 0x9C01, 0x8801, 0x4400,
+}
+
+func crc16(data []byte) uint16 {
+	var crc uint16 = 0xFFFF
+	for b := range data {
+		crc = (crc >> 4) ^ crcTable[crc&0x0F] ^ crcTable[b&0x0F]
+		crc = (crc >> 4) ^ crcTable[crc&0x0F] ^ crcTable[(b>>4)&0x0F]
+	}
+	return crc
+}
+
 func wrappedUpload(argv []string) {
 	names, _ := AssetDir("data")
 	sort.Strings(names)
@@ -199,7 +213,9 @@ func wrappedUpload(argv []string) {
 		fmt.Println("These firmware images are built-in:")
 		for i, name := range names {
 			info, _ := AssetInfo("data/" + name)
-			fmt.Printf("%3d: %-15s %5db\n", i+1, name, info.Size())
+			data, _ := Asset("data/" + name)
+			fmt.Printf("%3d: %-16s %5db  crc:%04X\n",
+				i+1, name, info.Size(), crc16(data))
 		}
 		fmt.Println("Use '!u <n>' to upload a specific one.")
 		return
